5cd0a2cdc6ea945ea33553adaf188650e1929777
Nilesh <nilesh@4rged.ai>
2026-08-15 11:23:17 +0530

    user-service.api update

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,27 +2,27 @@ import api from './api';
 
 const userService = {
   getUsers: async () => {
-    const { data } = await api.get('/users');
+    const { data } = await api.get('/api/users');
     return data;
   },
 
   createUser: async (userData) => {
-    const { data } = await api.post('/users', userData);
+    const { data } = await api.post('/api/users', userData);
     return data;
   },
 
   getUserById: async (id) => {
-    const { data } = await api.get(`/users/${id}`);
+    const { data } = await api.get(`/api/users${id}`);
     return data;
   },
 
   updateUser: async (id, userData) => {
-    const { data } = await api.put(`/users/${id}`, userData);
+    const { data } = await api.put(`/api/users${id}`, userData);
     return data;
   },
 
   deleteUser: async (id) => {
-    const { data } = await api.delete(`/users/${id}`);
+    const { data } = await api.delete(`/api/users${id}`);
     return data;
   },
 };
